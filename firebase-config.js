0312ecd4729961ef7145c6123ebf18d8a40fd5e5
sadogai2008 <oruto1157oruto2@gmail.com>
2026-06-06 20:51:09 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ============================================================
+// firebase-config.js
+// ============================================================
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDegjlrA3f0YaKD7-sdtHfwvfgb3NE17u4",
+  authDomain: "event-scheduler-401cb.firebaseapp.com",
+  projectId: "event-scheduler-401cb",
+  storageBucket: "event-scheduler-401cb.firebasestorage.app",
+  messagingSenderId: "581638129742",
+  appId: "1:581638129742:web:99e65adcbf1306339b9b1f",
+  measurementId: "G-JSNV1YN60Q"
+};
